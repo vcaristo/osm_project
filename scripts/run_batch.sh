9e04fc02cb14121d0936ccf497207e1ab8f0570f
Vince <89287198+vcaristo@users.noreply.github.com>
2025-04-03 16:40:43 -0600

    add email alerts to slurm job

--- scripts/run_batch.sh
+++ scripts/run_batch.sh
@@ -5,10 +5,13 @@
 #SBATCH --time=12:00:00
 #SBATCH --mem=16G
 #SBATCH --cpus-per-task=1
+#SBATCH --mail-type=all
+#SBATCH --mail-user=vc149353@umconnect.umt.edu
 
 # Load conda and activate environment
 module load conda
-conda activate ox
+
+conda activate us_osm_env
 
 # Run the script
 python download_osm_features.py
